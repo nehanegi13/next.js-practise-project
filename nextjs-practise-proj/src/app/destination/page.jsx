53d925c2683a453c74688ae507566c5eb9fe7354
Neha Negi <neha.negi0013@gmail.com>
2026-01-28 01:03:09 +0530

    updated

--- nextjs-practise-proj/src/app/destination/page.jsx
+++ nextjs-practise-proj/src/app/destination/page.jsx
@@ -1,7 +1,11 @@
+'use client'
+import { useRouter } from "next/navigation";
 import React from "react";
+
 
 const page = () => {
   const destination = ["Paris", "Tokyo", "NewYork"];
+  const router= useRouter()
   return (
     <div className="flex flex-col gap-4 justify-center items-center text-white  h-full">
       <div className="font-bold text-2xl mt-12 p-5">
@@ -12,7 +16,7 @@ const page = () => {
           return (
             <div
               key={index}
-              className="text-black font-bold text-2xl flex items-center justify-center rounded-2xl w-48 h-25 bg-white hover:scale-105 hover:bg-blue-200 cursor-pointer transition-all duration-300"
+              className="text-black font-bold text-2xl flex items-center justify-center rounded-2xl w-48 h-25 bg-white hover:scale-105 hover:bg-blue-200 cursor-pointer transition-all duration-300" onClick={() => router.push(`/destination/${d}`)}
             >
               {d}
             </div>
