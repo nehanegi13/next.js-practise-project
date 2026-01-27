871c8ce6260dd87579f2b90ff82b5e8bbd3ac67d
Neha Negi <neha.negi0013@gmail.com>
2026-01-27 23:41:30 +0530

    used usepathname hook for home, destination etc

--- nextjs-practise-proj/src/components/Navbar.jsx
+++ nextjs-practise-proj/src/components/Navbar.jsx
@@ -1,18 +1,27 @@
+"use client";
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 const Navbar = () => {
+  const pathname = usePathname();
   return (
-    <div className="w-full h-[80px] bg-white flex justify-between items-center px-17">
+    <div className="w-full h-20 bg-white flex justify-between items-center px-17">
       <div className="text-black font-bold text-2xl">🌍 Travel Guide</div>
       <div>
-        <ul className="flex justify-center items-center gap-[10px]">
-          <Link href={"/"}>
+        <ul className="flex justify-center items-center gap-10">
+          <Link href={"/"} className={pathname == "/" ? "text-blue-700" : ""}>
             <li>Home</li>
           </Link>
-          <Link href={"/destination"}>
+          <Link
+            href={"/destination"}
+            className={pathname == "/destination" ? "text-blue-700" : ""}
+          >
             <li>Destination</li>
           </Link>
-          <Link href={"/contact"}>
+          <Link
+            href={"/contact"}
+            className={pathname == "/contact" ? "text-blue-700" : ""}
+          >
             <li>Contact</li>
           </Link>
         </ul>
