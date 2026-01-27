6830169657dc023febfaf1d26ebcec96933b88bf
Neha Negi <neha.negi0013@gmail.com>
2026-01-28 01:03:35 +0530

    modified

--- nextjs-practise-proj/src/app/page.js
+++ nextjs-practise-proj/src/app/page.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>
-      
+    <div className=" flex justify-center items-center text-white font bold text-[40px] h-full">
+      Welcome To Our Travel Guide Website
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
