d79667b6824810f8cdc2f96b413ef7848bcc5a8d
Neha Negi <neha.negi0013@gmail.com>
2026-01-27 21:25:54 +0530

    imported Link from next/link

--- nextjs-practise-proj/src/components/Navbar.jsx
+++ nextjs-practise-proj/src/components/Navbar.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-
+import Link from "next/link";
 const Navbar = () => {
   return (
-    <div className="w-full h-[80px] bg-white">
+    <div className="w-full h-[80px] bg-white flex justify-between items-center px-17">
       <div className="text-black font-bold text-2xl">🌍 Travel Guide</div>
       <div>
-        <ul>
-          <li>Home</li>
-          <li>Destination</li>
-          <li>Contact</li>
+        <ul className="flex justify-center items-center gap-[10px]">
+          <Link href={"/"}>
+            <li>Home</li>
+          </Link>
+          <Link href={"/destination"}>
+            <li>Destination</li>
+          </Link>
+          <Link href={"/contact"}>
+            <li>Contact</li>
+          </Link>
         </ul>
       </div>
     </div>
