a5668a5b86975c6228cd6521108a0af6ee39c59e
Neha Negi <neha.negi0013@gmail.com>
2026-01-28 00:35:53 +0530

    map used

--- nextjs-practise-proj/src/app/destination/page.jsx
+++ nextjs-practise-proj/src/app/destination/page.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
+  const destination = ["Paris", "Tokyo", "NewYork"];
   return (
-    <div>page</div>
-  )
-}
+    <div className="flex flex-col gap-4 justify-center items-center text-white  h-full">
+      <div className="font-bold text-2xl mt-12 p-5">
+        Choose Your Destination
+      </div>
+      <div className="flex flex-col gap-4">
+        {destination.map((d, index) => {
+          return (
+            <div
+              key={index}
+              className="text-black font-bold text-2xl flex items-center justify-center rounded-2xl w-48 h-25 bg-white hover:scale-105 hover:bg-blue-200 cursor-pointer transition-all duration-300"
+            >
+              {d}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
