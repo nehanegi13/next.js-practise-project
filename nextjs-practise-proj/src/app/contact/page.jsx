8de97ca40d44ee10e7e66b786fba06880d5a8e88
Neha Negi <neha.negi0013@gmail.com>
2026-01-30 00:43:06 +0530

    edited contact page too by giving tailwind

--- nextjs-practise-proj/src/app/contact/page.jsx
+++ nextjs-practise-proj/src/app/contact/page.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div>contact page</div>;
+  return (
+    <div className="flex flex-col gap-4 justify-center items-center text-white  h-full">
+      contact page
+    </div>
+  );
 };
 
 export default page;
--- nextjs-practise-proj/src/app/destination/[city]/@info/page.jsx
+++ nextjs-practise-proj/src/app/destination/[city]/@info/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div className="text-white mt-45 fixed  ">parallel route</div>;
+  return <div className="text-white mt-45 fixed  "> </div>;
 };
 
 export default page;
