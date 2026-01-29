690ae23f38e249c4256788ba933a25194ba37d8b
Neha Negi <neha.negi0013@gmail.com>
2026-01-30 00:17:26 +0530

    imported Image tag from next

--- nextjs-practise-proj/src/app/destination/[city]/page.jsx
+++ nextjs-practise-proj/src/app/destination/[city]/page.jsx
@@ -4,10 +4,14 @@ import React from "react";
 import pairsimage from "@/assets/paris.jpg";
 import newyorkimage from "@/assets/newyork.avif";
 import tokyoimage from "@/assets/tokyo.jpg";
+import Image from "next/image";
 const page = ({ params }) => {
   const { city } = useParams();
   return (
-    <div className="text-white fixed   mt-25">{city} is the beautiful city</div>
+    <div className="text-white fixed   mt-25">
+      {city} is the beautiful city
+      <Image></Image>
+    </div>
   );
 };
 
